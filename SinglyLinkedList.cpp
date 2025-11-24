f274177ca41575f51e771ce52bdd03c21388d17b
Vivek Thapa <129367674+KevinGuitarist@users.noreply.github.com>
2025-11-24 20:30:56 +0530

    Add singly linked list implementation
    Implemented a singly linked list with four nodes and a display function.

---
+++ SinglyLinkedList.cpp
@@ -0,0 +1,37 @@
+//Understanding Node Structure
+/*
+class Node {
+    public:
+    int data;
+    Node* next;
+    Node(int data){
+        this->data = data;
+        this->next = nullptr;
+    }
+}
+*/
+
+//Creating an Example Linked List of Size 3
+
+#include <iostream>
+using namespace std;
+class Node {
+public:
+    int data;
+    Node* next;
+    Node(int new_data){
+        this -> data = new_data;
+        this -> next = nullptr;
+    }
+};
+int main(){
+    Node* head = new Node(10);
+    head -> next = new Node(20);
+    head -> next -> next = new Node(30);
+    head -> next -> next -> next = new Node(40);
+
+    while(head != nullptr){
+        cout << head -> data << " ";
+        head = head -> next;
+    }
+}
